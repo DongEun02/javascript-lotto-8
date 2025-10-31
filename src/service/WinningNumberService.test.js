23855ff0e94713ac2119ea7860b1a8ae55def762
김동은 <ehddms0517@naver.com>
2025-11-01 02:29:30 +0900

    test: Lotto 객체 생성 검증

--- src/service/WinningNumberService.test.js
+++ src/service/WinningNumberService.test.js
@@ -1,4 +1,5 @@
 import WinningNumberService from './WinningNumberService';
+import Lotto from '../domain/Lotto.js';
 
 describe('WinningNumberService', () => {
   test('쉼표로 구분된 문자열을 숫자 배열로 변환한다.', () => {
@@ -9,5 +10,12 @@ describe('WinningNumberService', () => {
   test('공백이 포함된 문자열도 올바르게 변환한다.', () => {
     const result = WinningNumberService.parse(' 1, 2 ,3,4 , 5,6 ');
     expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  test('입력 문자열로 Lotto 객체를 생성한다.', () => {
+    const lotto = WinningNumberService.createWinningLotto('1,2,3,4,5,6');
+
+    expect(lotto).toBeInstanceOf(Lotto);
+    expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
