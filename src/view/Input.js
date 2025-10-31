46f0e7b30a8a7e16d611007caaa6423340fb4470
김동은 <ehddms0517@naver.com>
2025-11-01 01:37:58 +0900

    fix: 입출력 줄바꿈 추가

--- src/view/Input.js
+++ src/view/Input.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 const Input = {
   async purchaseAmount() {
-    const input = await Console.readLineAsync('구입금액을 입력해 주세요\n');
+    const input = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     return Number(input);
   },
 };
--- src/view/Output.js
+++ src/view/Output.js
@@ -2,8 +2,9 @@ import { Console } from '@woowacourse/mission-utils';
 
 const Output = {
   printLottos(lottos) {
-    Console.print(`${lottos.length}개를 구매했습니다.`);
+    Console.print(`\n${lottos.length}개를 구매했습니다.`);
     lottos.forEach((lotto) => Console.print(lotto));
+    Console.print('');
   },
 };
 
