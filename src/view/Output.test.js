5b386de1aa56813bf19c18efd30b89b818e48963
김동은 <ehddms0517@naver.com>
2025-11-02 02:11:05 +0900

    test: 당첨 내역 출력 검증

--- src/view/Output.test.js
+++ src/view/Output.test.js
@@ -23,4 +23,34 @@ describe('Output 모듈 테스트', () => {
     expect(Console.print).toHaveBeenNthCalledWith(2, lottos[0]);
     expect(Console.print).toHaveBeenNthCalledWith(3, lottos[1]);
   });
+
+  test('출력 순서와 내용이 올바르다', () => {
+    const result = {
+      FIRST: 1,
+      SECOND: 2,
+      THIRD: 3,
+      FOURTH: 4,
+      FIFTH: 5,
+      NONE: 0,
+    };
+    const profitRate = '62.5';
+
+    Output.printResult(result, profitRate);
+
+    const expectedCalls = [
+      '\n당첨통계\n---',
+      `3개 일치 (5,000원) - 5개`,
+      `4개 일치 (50,000원) - 4개`,
+      `5개 일치 (1,500,000원) - 3개`,
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - 2개`,
+      `6개 일치 (2,000,000,000원) - 1개`,
+      `총 수익률은 62.5%입니다.`,
+    ];
+
+    expect(Console.print).toHaveBeenCalledTimes(expectedCalls.length);
+
+    expectedCalls.forEach((text, index) => {
+      expect(Console.print).toHaveBeenNthCalledWith(index + 1, text);
+    });
+  });
 });
